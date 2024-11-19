3bd72a6ab492d3848238d36ba9adae60d02655d5
AnmolPandey009 <pandeyanmol0909@gmail.com>
2024-11-19 18:40:49 +0530

    Add files via upload

---
+++ src/hooks/useItems.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const useItems = () => {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=11", { mode: "cors" })
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error("server error");
+        }
+
+        return res.json();
+      })
+      .then((json) => {
+        // Add the isAdded key to each item
+        const updatedItems = json.map((item) => ({
+          ...item,
+          isAdded: false, // Initial value
+          qty: 0,
+        }));
+
+        setItems(updatedItems);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { items, setItems, error, loading };
+};
+
+export default useItems;
