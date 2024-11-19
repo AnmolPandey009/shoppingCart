f138897cfc9acf74f52a37e27ccea6060095c20b
AnmolPandey009 <pandeyanmol0909@gmail.com>
2024-11-19 18:56:15 +0530

    Add files via upload

---
+++ src/components/errorPage/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <main className={styles.errorContainer}>
+        <h1 className={styles.errorTitle}>Oops!</h1>
+        <p className={styles.errorText}>Page Not Found</p>
+        <Link to="/">
+          <button className={styles.errorBtn}>Back To Home</button>
+        </Link>
+      </main>
+    </>
+  );
+};
+
+export default ErrorPage;
